ee3f2fa0e988f68727f918370f281fc628dfd89b
root <root@LAPTOP-3DJSV2GR.localdomain>
2021-10-09 13:24:53 -0300

    chore: :bug: alte

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := Soma(15, 15)
+	total := soma(15, 15)
 
 	if total != 30 {
 		t.Errorf("o resultado da da soma é invalido resultado %d esperado %d", total, 30)
